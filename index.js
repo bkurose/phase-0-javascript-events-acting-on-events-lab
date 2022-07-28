f32dc6bf665f2e2cd140a29694f47340f8b7b287
Ben Kurose <ben.kurose@gmail.com>
2022-07-28 12:54:33 -0400

    commit tree 94966f10c613935842d8f219011de0a64e49cd61

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+function moveDodgerRight() {
+    const rightNumber = dodger.style.left.replace("px", "");
+    const right = parseInt(rightNumber, 10);
+
+    if (right < 380){
+        dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener("keydown", function (f) {
+    if (f.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+})
